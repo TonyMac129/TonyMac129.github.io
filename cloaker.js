72dc1b9ccb7613d6fae2102cb0f83bdd0230a183
Tony Macaroni <128721120+TonyMac129@users.noreply.github.com>
2023-07-10 17:21:22 -0400

    Added tab cloaker feature!

---
+++ cloaker.js
@@ -0,0 +1,28 @@
+function cloaking(){
+  const name = document.getElementById("name").value;
+  localStorage.cloak = name;
+}
+function google() {
+  localStorage.cloak = "Google"
+}
+function canva() {
+  localStorage.cloak = "Canva"
+}
+function clever() {
+  localStorage.cloak = "Clever"
+}
+function schoology() {
+  localStorage.cloak = "Schoology"
+}
+function newTab() {
+  localStorage.cloak = "New Tab"
+}
+
+function changeName() {
+  if (localStorage.cloak!=null) {
+    const title = document.querySelector(".cloak");
+  title.innerHTML = localStorage.cloak;
+  } 
+}
+
+setInterval(changeName,100);
